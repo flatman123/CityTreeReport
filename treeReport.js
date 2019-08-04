c30e9f377ce71ace6738fd2912d4ed1b300ce555
Jeffrey Mcintyre <flatlandphyon67@holmail.com>
2019-08-04 08:39:11 -0700

    Removed commented text

--- treeReport.js
+++ treeReport.js
@@ -1,29 +1,4 @@
-/* Suppose That you're working in a small town administration, and you in charge of 
-two elements:
-
-1. Parks
-2 Streets
-
-It's a very small town, so right now there are only 3 parks and 4 streets. All parks have streets
-have a name and a build year.
-
-At an end-of-year meeting, your boss wants a final report with the following:
-
-1. tree density of each park in the town ( formula: number of trees/park area)
-
-2. Average age of each town's park (formula: sum of all ages/number of parks);
-
-3. The name of the park that has more thnan 1000 trees
-
-4. total and average length of the towns's streets.
-
-5 Size classification of all streets: tiny/small/normal/big/huge/ If the size is unknown,
-then the default is normal.
-
-Report should be printed to console.*/
-
 // ES6 version
-
 
 const brooklyn = new Map();
 
