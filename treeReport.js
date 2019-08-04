4b7523d96bb5469718d505c297e46c8d2803c4cd
Jeffrey Mcintyre <flatlandphyon67@holmail.com>
2019-08-04 08:36:08 -0700

    added getTreeAge method. refactored code for printAverageAgeOfParks method. added subclass

--- treeReport.js
+++ treeReport.js
@@ -28,24 +28,21 @@ Report should be printed to console.*/
 const brooklyn = new Map();
 
 brooklyn.set('Prospect Park',{
-	trees:20,
+	trees:60,
 	year:1988,
-	parkArea: 5001,
+	parkArea: 100,
 	streets: {
-
 		street1: ['Flatbush Ave', 1888],
 		street2: ['Caton Ave', 1777],
 		street3: ['Church Ave', 1874]
 	}
-
 });
 
 brooklyn.set('GroverClevland Park', {
-	trees:15,
+	trees:32,
 	year: 1930,
 	parkArea: 1021,
 	streets: {
-
 		street1: ['Green Street', 1812],
 		street2: ['Stone Ave', 1984],
 		street3: ['Auburn Road', 1988]
@@ -55,24 +52,23 @@ brooklyn.set('GroverClevland Park', {
 brooklyn.set('DR Park', {
 	trees: 40, 
 	year:1960,
-	parkArea: 811,
+	parkArea: 200,
 	streets: {
-
 		street1: ['Flatland Ave',1545],
 		street2: ['Renson Ave', 1455],
 		street3: ['Avenue U', 1744]
 	}
 });
 
-
 class CompileYearReport {
-	constructor (parkName) {
+	constructor (parkName, reportName, reportID) {
 		this.parkName = parkName;
+		this.reportName = reportName;
+		this.reportID = Math.floor(Math.random() * 999);
 	}
 
-	calcDensity() {
+	calcDensity() {		
 		let numOfTrees, parkArea, street;
-
 		numOfTrees = brooklyn.get(this.parkName).trees;
 		parkArea = brooklyn.get(this.parkName).parkArea;
 		this.treeDensity = numOfTrees / parkArea;
@@ -80,21 +76,24 @@ class CompileYearReport {
 		console.log(`${this.parkName} has a tree density of ${this.treeDensity}.`)
 	}
 
+	getTreeAge() {
+		let currentYear, age1, age2, age3;
+		currentYear = new Date();
+		return {
+			parkAge1: currentYear.getFullYear() - brooklyn.get('Prospect Park').year,
+			parkAge2: currentYear.getFullYear() - brooklyn.get('GroverClevland Park').year,
+			parkAge3: currentYear.getFullYear() - brooklyn.get('DR Park').year
+		}
+	}
+
 	printAverageAgeOfParks() {
-		let park1, park2, park3;
-
-		park1 = brooklyn.get('Prospect Park').year;
-		park2 = brooklyn.get('GroverClevland Park').year;
-		park3 = brooklyn.get('DR Park').year;
-
-
-		let averageAge = (park1 + park2 + park3) / brooklyn.size;
-
-		console.log(`Our ${brooklyn.size} has an average of ${averageAge} years.`);
-
+		let averageAge, treeAge;
+		averageAge = Math.floor((this.getTreeAge().parkAge1 + this.getTreeAge().parkAge2 + this.getTreeAge().parkAge2) / brooklyn.size);
+		console.log(`Our ${brooklyn.size} parks have an average age of ${averageAge} years.`);
 	}
 
 	greaterThanOneThousand() {
+
 
 	}
 
@@ -103,12 +102,10 @@ class CompileYearReport {
 	}
 }
 
-
-class PrintOutYearReport {
-	constructor (reportName);
-
+class PrintOutYearReport extends CompileYearReport {
+	constructor(parkName, reportName, reportID) {
+		super(parkName, reportName, reportID);
+	}
 }
-
-
-
-const ProspectPark = new CompileYearReport('Prospect.P_2019_Report','Prospect Park');
+const ProspectPark1 = new PrintOutYearReport('Prospect Park','Prospect.P_2019_Report');
+const ProspectPark2 = new PrintOutYearReport('Prospect Park','Prospect.P_2019_Report');
